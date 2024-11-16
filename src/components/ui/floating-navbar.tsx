39c2c5baad5098b6e8c4ff366ec625ced4819243
Reisel Valle <reiselvalle@gmail.com>
2024-11-16 12:02:15 -0500

    fix navbar

---
+++ src/components/ui/floating-navbar.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const FloatingNav = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(true);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      const direction = current - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        // also set true for the initial state
+        setVisible(true);
+      }
+
+      if (direction < 0) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    }
+  });
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-8 py-4 rounded-full bg-transparent border border-golden-100 hover:border-golden-200 shadow-md shadow-dark-200 hover:shadow-dark-100 items-center justify-center space-x-6 transition-all backdrop-blur-lg",
+          className
+        )}
+      >
+        {navItems.map(({ link, name }) => (
+          <Link
+            key={`link=${name}`}
+            href={link}
+            className={cn(
+              "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+          >
+            {/* <span className="block sm:hidden">{icon}</span> */}
+            <span className="text-sm !cursor-pointer text-yellow-50 hover:text-golden-200 transition-all">
+              {name}
+            </span>
+          </Link>
+        ))}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
---
+++ src/lib/constants/menu.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { name: "Sobre mí", link: "#about" },
+  { name: "Proyectos", link: "#projects" },
+  { name: "Experiencia", link: "#experience" },
+  { name: "Contacto", link: "#contact" },
+];
