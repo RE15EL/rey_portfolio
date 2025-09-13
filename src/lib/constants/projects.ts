a9a07da6847c194af35f14978ea14810f9848fc6
Reisel Valle <reiselvalle@gmail.com>
2025-09-13 16:32:42 -0400

    add Morereps project

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -3,20 +3,20 @@ import { IProject } from "@/types";
 export const projects: IProject[] = [
   {
     id: 1,
-    title: "Articulación Digital",
-    des: "Portal web para servicios de agendamiento de Vitalmex.",
-    img: "/images/art_digital.png",
+    title: "Morereps App",
+    des: "App de planificación y seguimiento de rutinas, motivación, progreso y nutrición.",
+    img: "/images/morereps_app.png",
     iconLists: [
       "/images/next.svg",
       "/images/ts.svg",
       "/images/tail.svg",
       "/images/fm.svg",
     ],
-    link: "http://front.coolray.com.mx/",
+    link: "#",
   },
   {
     id: 2,
-    title: "Taskmind - Video Conferencing App",
+    title: "Taskmind",
     des: "TaskMind es una app web de productividad diseñada para ayudar a los usuarios a gestionar tareas eficientemente.",
     img: "/images/taskmind.png",
     iconLists: [
@@ -29,7 +29,7 @@ export const projects: IProject[] = [
   },
   {
     id: 3,
-    title: "Reto Viral - 12345678",
+    title: "Reto Viral (12345678)",
     des: "El Reto Viral de 123456789 es un desafío de programación que demuestra la manipulación avanzada de cadenas y matrices mediante patrones visuales de dígitos.",
     img: "/images/reto_viral.png",
     iconLists: [
@@ -42,8 +42,8 @@ export const projects: IProject[] = [
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    title: "Articulación Digital",
+    des: "Portal web para servicios de agendamiento de Vitalmex.",
     img: "/images/art_digital.png",
     iconLists: [
       "/images/next.svg",
@@ -51,6 +51,6 @@ export const projects: IProject[] = [
       "/images/tail.svg",
       "/images/fm.svg",
     ],
-    link: "#",
+    link: "http://front.coolray.com.mx/",
   },
 ];
