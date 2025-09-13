efefc0c4b550e6113e4fea4fe7474ae9906cc621
Reisel Valle <reiselvalle@gmail.com>
2025-09-12 21:45:51 -0400

    fix: useEffects issues

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,5 @@ export * from './background-beams-with-collision';
 export * from './background-gradient';
 export * from './bento-grid';
 export * from './floating-navbar';
-export * from './footer';
 export * from './magic-button';
 export * from './typewriter-effect';
