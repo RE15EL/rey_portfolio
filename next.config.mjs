52814c66e9c8f70687b78c1408d947d9c7c32484
Reisel Valle <reiselvalle@gmail.com>
2025-09-12 22:06:27 -0400

    optimized images - add sharp dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    //* Permitir dominios externos
+    remotePatterns: [
+      //   {
+      //     protocol: "https",
+      //     hostname: "avatars.githubusercontent.com", // ejemplo GitHub
+      //   },
+    ],
+
+    //* Habilitar formatos modernos de compresión
+    formats: ["image/avif", "image/webp"],
+
+    //? Opcional: tamaños que usará next/image
+    deviceSizes: [320, 640, 768, 1024, 1280, 1536],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
+
+  //? Opcional: elimina el header "X-Powered-By"
+  poweredByHeader: false,
+};
 
 export default nextConfig;
